17d2380d3789f3f248ae232a95bae32dfe09caae
Stanislav-Zhuk <steys.ua@gmail.com>
2023-02-20 17:13:52 +0200

    update task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+
+ingredientsList.append(...elements);
